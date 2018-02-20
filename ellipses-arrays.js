92ee042c75937206fdbb4bf006eba5f7e2426d6c
Kailin Li <35666977+kailinandcode@users.noreply.github.com>
2018-02-20 15:33:51 -0800

    Update ellipses-arrays.js

--- ellipses-arrays.js
+++ ellipses-arrays.js
@@ -1,13 +1,5 @@
 /*modify the code below, adding arrays to the data model, so that there are 50 different circles arrayed around the canvas.
 Bonus points for having that pattern be interesting in some way.*/
-var x1 = 100;
-var y1 = 200;
-var d1 = 100;
-
-var x2 = 300;
-var y2 = 200;
-var d2 = 30;
-
 var xArr = [];
 var yArr = [];
 var colorArr = [];
@@ -29,25 +21,25 @@ function draw() {
   noStroke();
 
   // draw fifty ellipses
-	for (var i = 0; i < 50; i += 1) {
+  for (var i = 0; i < 50; i += 1) {
 	stroke(i);
-  fill(120+colorArr[i], colorArr[i], 150);
-  ellipse(xArr[i], yArr[i], dArr[i]);
+	fill(colorArr[i], colorArr[i], 150);
+  	ellipse(xArr[i], yArr[i], dArr[i]);
   
 	stroke(i+1);
-  fill(180+colorArr[i], colorArr[i+1], 150);
-  ellipse(xArr[i], yArr[i], dArr[i+1]);
+ 	fill(colorArr[i], colorArr[i+1], 150);
+  	ellipse(xArr[i], yArr[i], dArr[i+1]);
   
   // 1% of the time
-  if (random() < 0.01) {
-    // random diameter between 10 and 400
-    dArr[i] = random(10, 200);
-  }
+ 	 if (random() < 0.01) {
+    // random diameter between 10 and 100
+   	 dArr[i] = random(10, 100);
+ 	 }
   
   // 2% of the time…
-  if (random() < 0.02) {
-    // random diameter between 10 and 400
-    dArr[i+1] = random(10, 200);
+ 	 if (random() < 0.02) {
+    // random diameter between 10 and 200
+    	dArr[i+1] = random(10, 200);
+ 	 }
   }
-	}
 }
