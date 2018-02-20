96046f56557f9cdd6d6698c2178cdaa714621272
Kailin Li <35666977+kailinandcode@users.noreply.github.com>
2018-02-20 15:06:06 -0800

    Update water-array.js

--- water-array.js
+++ water-array.js
@@ -3,14 +3,14 @@
 /*adding arrays to the data model, so that there are many independent drops visible at any given time
 Consider giving each drop a small, random x and y speed. What range works well for those speeds?*/
 
-var xArr = [];
-var yArr = [];
+var x = [];
+var y = [];
 
 function setup() {
   createCanvas(400, 400);
-	for (var index = 0; index < 100; index = index + 1) {
-    xArr[index] = random(10, width-10);
-    yArr[index] = random(10, height-10);
+	for (var index = 0; index < 200; index = index + 1) {
+    x[index] = random(10, width-10);
+    y[index] = random(10, height-10);
   }
 }
   
@@ -20,18 +20,16 @@ function draw() {
   noStroke();
 
   // draw pipe
-  
+  rect(0, 200, 230, 20);
+	
   // draw drip
-	for (var index = 0; index < 3; index = index + 1) {
-		rect(0, 200, 230, 20);
-    ellipse(xArr[index], yArr[index], 10);
-    yArr[index] = yArr[index] + 4;
-
-  // down 3 pixels each frame, but maybe should be accelerating?
-  // if invisible for a full “height” amount…
-  	if (yArr > height*2) {
-    // reset
-    yArr = 220;
-  	}
-	}
+  for (var index = 0; index < 100; index = index + 1) {
+    		ellipse(x[index], y[index], 10);
+		if (y[index] > height*2) {
+   		 // reset
+   		 y[index] = 220;
+  		}
+    y[index] = y[index+1] * (1 + y[index])/y[index];
+    x[index] = x[index+1] * (1 + x[index])/x[index];
+  }
 }
