d31d9f1e6ea362479fa1fb3bd5e87204903a698c
Kailin Li <35666977+kailinandcode@users.noreply.github.com>
2018-02-20 18:42:57 -0800

    Update smoke-arrays.js

--- smoke-arrays.js
+++ smoke-arrays.js
@@ -1,20 +1,21 @@
 
 /* adding arrays to the data model, so that there are 100 squares rotating and rising. 
 Consider adding additional data to the model: perhaps an xSpeed, judiciously used, will give the smoke some volume?*/
-var x = 210;
-var y = 50;
+
 var r = 0;
 
 var rectangles = [];
-var positions = [];
+var xPositions = [];
+var yPositions = [];
 var xSpeed = [];
 var ySpeed = [];
 
 function setup() {
   createCanvas(400, 400);
 	for (i = 0; i < 100; i +=1) {
-		rectangles[i] = random(10, 40);
-		positions[i] = random(-10, 10);
+		yPositions[i] = random(10, height-10);
+		xPositions[i] = random(10, width-10);
+		rectangles[i] = random(10, 30);
 		xSpeed[i] = random(-5, 5);
 		ySpeed[i] = random(-5, 5);
 	}
@@ -31,30 +32,32 @@ function draw() {
   // save graphics state
   push();
   // darker as it gets closer to 0
-  fill(y);
+  
   // rotate r around (x,y)
   translate(x, y);
   rotate(r);
+	  
+	// reset rotation and translation
+  pop();
+	
   // draw rectangle
 	for (i = 0; i < 100; i++) {
-		
-  rect(positions[i], positions[i], rectangles[i], rectangles[i]);
-	
-  // reset rotation and translation
-  pop();
+	fill(xPositions[i]);
+  rect(xPositions[i], yPositions[i], rectangles[i], rectangles[i]);
+
   
-  positions[i] = 
+  xPositions[i] = xPositions[i] + xSpeed[i];
+  yPositions[i] = yPositions[i] + ySpeed[i];
 		
   // rotate 0.05 radians ~= 2.8 degrees per frame
   r += 0.05;
-  
-  // if reach past the top a bunch
-  if (rectangles[i] < -150) {
-    rectangles[i] = 290;
-  }
 	
-	if (rectangles[i] < -150) {
-		rectangles[i] = 210;
+  // if reach past the top a bunch
+  if (xPositions[i] >= height) {
+		xPositions[i] = 10;
+	}
+	if (yPositions[i] >= width) {
+		yPositions[i] = 10;
 	}
 	}
 }
